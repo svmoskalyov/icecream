6eb1568a24bb19b81de4ef36dbc48dd2c5a493d2
Sviatoslava13 <sviatoslavamar@gmail.com>
2022-08-30 01:09:07 +0300

    new work

--- src/index.js
+++ src/index.js
@@ -61,3 +61,17 @@ $('.nav__link').click(function () {
   $('.page-header__btn').removeClass('active');
   $('body').removeClass('overflow');
 });
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-map-open]"),
+    closeModalBtn: document.querySelector("[data-modal-map-close]"),
+    modal: document.querySelector("[data-modal-map]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden-map");
+  }
+})();
