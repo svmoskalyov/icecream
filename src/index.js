2438c23e41dbe330f26fa9696b1a19e6a6376c83
vitaliksmile <vitaliksmile777@gmail.com>
2022-08-31 20:35:11 +0300

    Зробив модальне вікно about

--- src/index.js
+++ src/index.js
@@ -1,58 +1,58 @@
-$(".reviews__slider").slick({
-    infinite: true,
-    arrows: false,
-    autoplay: true,
-    dots: true,
+$('.reviews__slider').slick({
+  infinite: true,
+  arrows: false,
+  autoplay: true,
+  dots: true,
 });
 
 (() => {
-  const menuBtnRef = document.querySelector("[data-menu-button]");
-  const mobileMenuRef = document.querySelector("[data-menu]");
-  const mobilePhBtn = document.querySelector("[data-ph-btn]");
-  
-  menuBtnRef.addEventListener("click", () => {
+  const menuBtnRef = document.querySelector('[data-menu-button]');
+  const mobileMenuRef = document.querySelector('[data-menu]');
+  const mobilePhBtn = document.querySelector('[data-ph-btn]');
+
+  menuBtnRef.addEventListener('click', () => {
     const expanded =
-      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+      menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
 
-    menuBtnRef.classList.toggle("is-open");
-    menuBtnRef.setAttribute("aria-expanded", !expanded);
+    menuBtnRef.classList.toggle('is-open');
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
 
-    mobileMenuRef.classList.toggle("is-open");
+    mobileMenuRef.classList.toggle('is-open');
     document.body.classList.toggle('overflow');
-    mobilePhBtn.classList.toggle("active");
+    mobilePhBtn.classList.toggle('active');
   });
 })();
 
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    document.body.classList.toggle("modal-open");
-    refs.modal.classList.toggle("is-hidden");
+    document.body.classList.toggle('modal-open');
+    refs.modal.classList.toggle('is-hidden');
     document.body.classList.toggle('overflow');
   }
 })();
 
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-cooperation-open]"),
-    closeModalBtn: document.querySelector("[data-modal-cooperation-close]"),
-    modal: document.querySelector("[data-modal-cooperation]"),
+    openModalBtn: document.querySelector('[data-modal-cooperation-open]'),
+    closeModalBtn: document.querySelector('[data-modal-cooperation-close]'),
+    modal: document.querySelector('[data-modal-cooperation]'),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-hidden-cooperation");
-        document.body.classList.toggle('overflow');
+    refs.modal.classList.toggle('is-hidden-cooperation');
+    document.body.classList.toggle('overflow');
   }
 })();
 
@@ -64,16 +64,32 @@ $('.nav__link').click(function () {
 });
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-map-open]"),
-    closeModalBtn: document.querySelector("[data-modal-map-close]"),
-    modal: document.querySelector("[data-modal-map]"),
+    openModalBtn: document.querySelector('[data-modal-map-open]'),
+    closeModalBtn: document.querySelector('[data-modal-map-close]'),
+    modal: document.querySelector('[data-modal-map]'),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-hidden-map");
-        document.body.classList.toggle('overflow');
+    refs.modal.classList.toggle('is-hidden-map');
+    document.body.classList.toggle('overflow');
+  }
+})();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-about-open]'),
+    closeModalBtn: document.querySelector('[data-modal-about-close]'),
+    modal: document.querySelector('[data-modal-about]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden-about');
+    document.body.classList.toggle('overflow');
   }
 })();
